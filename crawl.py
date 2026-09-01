0d5ef85580b5b2eef348ddb1955fe781ddf80137
Jeerapas Kwansiri <jeerapas.kwansiri@gmail.com>
2026-09-02 00:56:57 +0700

    add extract_page_data function with unittest

--- crawl.py
+++ crawl.py
@@ -1,5 +1,14 @@
 from urllib.parse import urlsplit, urljoin
 from bs4 import BeautifulSoup, Tag
+from typing import TypedDict
+
+
+class PageData(TypedDict):
+    url: str
+    heading: str
+    first_paragraph: str
+    outgoing_links: list[str]
+    image_urls: list[str]
 
 
 def normalize_url(url: str) -> str:
@@ -63,3 +72,13 @@ def get_images_from_html(html, base_url):
                 print(f"Error joining URL -> {str(e)}: {src}")
 
     return result
+
+
+def extract_page_data(html: str, page_url: str) -> PageData:
+    return {
+        "url": page_url,
+        "heading": get_heading_from_html(html),
+        "first_paragraph": get_first_paragraph_from_html(html),
+        "outgoing_links": get_urls_from_html(html, page_url),
+        "image_urls": get_images_from_html(html, page_url),
+    }
