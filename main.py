0f04c38ab4ffbd15a4b8360b80493562ddfd9ed0
Jeerapas Kwansiri <jeerapas.kwansiri@gmail.com>
2026-09-02 15:30:04 +0700

    add get_html call to main entry point

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
+import sys
+from crawl import get_html
+
+
 def main():
-    print("Hello from web-crawler!")
+    if len(sys.argv) < 2:
+        print("no website provided")
+        sys.exit(1)
+    if len(sys.argv) > 2:
+        print("too many arguments provided")
+        sys.exit(1)
+
+    base_url = sys.argv[1]
+    print(f"starting crawl of: {base_url}")
+
+    try:
+        html_str = get_html(base_url)
+    except Exception as e:
+        print(f"Error fetching HTML from {base_url}: {str(e)}")
+        sys.exit(1)
+
+    print(html_str)
+
+    sys.exit(0)
 
 
 if __name__ == "__main__":
