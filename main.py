ca81cc1b5ff8b624194560dc78131487dde109fb
Jeerapas Kwansiri <jeerapas.kwansiri@gmail.com>
2026-09-02 22:01:12 +0700

    refactor(main): call crawl_page instead of get_html

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
-from crawl import get_html
+from crawl import crawl_page
+from rich import print
 
 
 def main():
@@ -13,13 +14,16 @@ def main():
     base_url = sys.argv[1]
     print(f"starting crawl of: {base_url}")
 
-    try:
-        html_str = get_html(base_url)
-    except Exception as e:
-        print(f"Error fetching HTML from {base_url}: {str(e)}")
-        sys.exit(1)
+    page_data = crawl_page(base_url)
 
-    print(html_str)
+    print("------------")
+
+    print(f"Found {len(page_data)} pages:")
+    for page in page_data.values():
+        print(f"- {page['url']}: {len(page['outgoing_links'])} outgoing links")
+
+    print("------------")
+    print(page_data)
 
     sys.exit(0)
 
